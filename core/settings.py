cf9289894b8bf268b4126f2e8d5610fca67ff7fa
j99i <jortega7785@gmail.com>
2026-02-20 13:12:59 -0600

    Sprint 4: backup automático con apscheduler

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
 # --- CLOUDINARY solo para archivos subidos por usuarios (media) ---
 CLOUDINARY_STORAGE = {
